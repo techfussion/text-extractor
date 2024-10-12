1eaab693501bdbb6ab652ee26a02b65ca88b1b2e
techfussion <muminraj15@gmail.com>
2024-10-12 14:28:04 +0100

    Initial commit with splash screen

---
+++ app/src/main/java/com/example/textextractor/SplashScreen.kt
@@ -0,0 +1,32 @@
+package com.example.textextractor
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_onboarding_screen1)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val onboarding = Intent(this, OnboardingScreens::class.java)
+            startActivity(onboarding)
+            finish()
+        }, 3000)
+    }
+}
