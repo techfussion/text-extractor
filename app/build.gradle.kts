24d7a7d0445f281378eb06e624541a3f9c34a49e
techfussion <muminraj15@gmail.com>
2024-10-13 23:05:20 +0100

    completed onboarding screen structure

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,4 +60,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("com.github.msayan:tutorial-view:v1.0.10")
 }
--- app/src/main/java/com/example/textextractor/OnboardingScreens.java
+++ app/src/main/java/com/example/textextractor/OnboardingScreens.java
@@ -1,5 +1,7 @@
 package com.example.textextractor;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,115 +9,52 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.viewpager2.widget.ViewPager2;
-import android.content.Intent;
-import android.text.Html;
-import android.view.View;
-import android.view.WindowManager;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
-import com.taimoorsikander.cityguide.HelperClasses.SliderAdapter;
-import com.taimoorsikander.cityguide.R;
-import com.taimoorsikander.cityguide.User.UserDashboard;
+import com.hololo.tutorial.library.Step;
+import com.hololo.tutorial.library.TutorialActivity;
 
-
-public class OnboardingScreens extends AppCompatActivity {
-    //Variables
-    ViewPager2 viewPager2;
-    LinearLayout dotsLayout;
-    SliderAdapter sliderAdapter;
-    TextView[] dots;
-    Button letsGetStarted;
-    Animation animation;
-    int currentPos;
+public class OnboardingScreens extends TutorialActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_on_boarding);
-        setContentView(R.layout.activity_onboarding_screens);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
-        //Hooks
-        viewPager2 = findViewById(R.id.slider);
-        dotsLayout = findViewById(R.id.dots);
-        letsGetStarted = findViewById(R.id.get_started_btn);
+        addFragment(new Step.Builder().setTitle("This is header")
+                .setContent("This is content")
+                .setBackgroundColor(Color.parseColor("#FF0957")) // int background color
+                .setDrawable(R.drawable.undraw_convert_re_l0y1) // int top drawable
+                .setSummary("This is summary")
+                .build());
 
-        //Call adapter
-        sliderAdapter = new SliderAdapter(this);
-        viewPager2.setAdapter(sliderAdapter);
+        addFragment(new Step.Builder().setTitle("This is header")
+                .setContent("This is content")
+                .setBackgroundColor(Color.parseColor("#FF0957")) // int background color
+                .setDrawable(R.drawable.undraw_convert_re_l0y1) // int top drawable
+                .setSummary("This is summary")
+                .build());
 
-        //Dots addOnPageChangeListener
-        addDots(0);
-        viewPager2.addOnLayoutChangeListener(changeListener);
+        addFragment(new Step.Builder().setTitle("This is header")
+                .setContent("This is content")
+                .setBackgroundColor(Color.parseColor("#FF0957")) // int background color
+                .setDrawable(R.drawable.undraw_convert_re_l0y1) // int top drawable
+                .setSummary("This is summary")
+                .build());
     }
 
-    public void skip(View view) {
-        startActivity(new Intent(this, UserDashboard.class));
-        finish();
+    @Override
+    public void finishTutorial() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
-    public void next(View view) {
-        viewPager2.setCurrentItem(currentPos + 1);
-    }
-
-    private void addDots(int position) {
-
-        dots = new TextView[4];
-        dotsLayout.removeAllViews();
-
-        for (int i = 0; i < dots.length; i++) {
-            dots[i] = new TextView(this);
-            dots[i].setText(Html.fromHtml("&#8226;"));
-            dots[i].setTextSize(35);
-
-            dotsLayout.addView(dots[i]);
-        }
-
-        if (dots.length > 0) {
-            dots[position].setTextColor(getResources().getColor(R.color.colorPrimaryDark));
-        }
+    @Override
+    public void currentFragmentPosition(int position) {
 
     }
 
-    ViewPager2.OnPageChangeCallback changeListener = new ViewPager2.OnPageChangeCallback() {
-        @Override
-        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-        }
-
-        @Override
-        public void onPageSelected(int position) {
-            addDots(position);
-            currentPos = position;
-
-            if (position == 0) {
-                letsGetStarted.setVisibility(View.INVISIBLE);
-            } else if (position == 1) {
-                letsGetStarted.setVisibility(View.INVISIBLE);
-            } else if (position == 2) {
-                letsGetStarted.setVisibility(View.INVISIBLE);
-            } else {
-                animation = AnimationUtils.loadAnimation(OnBoarding.this, R.anim.bottom_anim);
-                letsGetStarted.setAnimation(animation);
-                letsGetStarted.setVisibility(View.VISIBLE);
-            }
-
-        }
-
-        @Override
-        public void onPageScrollStateChanged(int state) {
-
-        }
-
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
     }
+}
